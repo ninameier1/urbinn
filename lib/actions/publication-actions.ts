a9fa6d6b95d9016bd2d91d1e6b3ef5da58acaf9d
ninameier1 <s1081087@student.windesheim.nl>
2026-05-23 14:43:00 +0200

    Feature: Publication CRUD server actions

---
+++ lib/actions/publication-actions.ts
@@ -0,0 +1,88 @@
+'use server'
+
+import { prisma } from '@/lib/prisma'
+import { requireAuth } from '@/lib/auth/auth-guard'
+import { PublicationSchema } from '@validations/zodSchemas'
+
+// ------- PUBLICATION CRUD --------
+
+// create
+export async function createPublication(formData: FormData) {
+  const session = await requireAuth()
+  const userId = Number(session.user.id)
+
+  const parsed = PublicationSchema.safeParse({
+    title: formData.get('title'),
+    author: formData.get('author'),
+    description: formData.get('description'),
+    url: formData.get('url'),
+    published_at: formData.get('published_at'),
+    municipality_id: formData.get('municipality_id')
+  })
+
+  if (!parsed.success) {
+  throw new Error(parsed.error.issues[0].message)
+  }
+
+  const publication = await prisma.publication.create({
+    data: {
+      ...parsed.data,
+      created_by: userId,
+    },
+  });
+  return publication.id;
+}
+
+// read
+export async function getPublication(id: number) {
+  await requireAuth()
+
+  return await prisma.publication.findUnique({ where: { id } })
+}
+
+// read all
+export async function getAllPublications() {
+  await requireAuth()
+  return await prisma.publication.findMany()
+}
+
+// read all by municipality
+export async function getPublicationsByMunicipality(municipalityId: number) {
+  await requireAuth()
+
+  return await prisma.publication.findMany({
+    where: { municipality_id: municipalityId },
+  })
+}
+
+// update
+export async function updatePublication(id: number, formData: FormData) {
+  await requireAuth()
+
+  const parsed = PublicationSchema.safeParse({
+    title: formData.get('title'),
+    author: formData.get('author'),
+    description: formData.get('description'),
+    url: formData.get('url'),
+    published_at: formData.get('published_at'),
+    municipality_id: formData.get('municipality_id')
+  })
+  
+  if (!parsed.success) {
+  throw new Error(parsed.error.issues[0].message)
+  }
+
+  await prisma.publication.update({
+    where: { id },
+    data: { 
+      ...parsed.data, 
+    },
+  })
+}
+
+// delete
+export async function deletePublication(id: number) {
+  await requireAuth()
+
+  await prisma.publication.delete({ where: { id } })
+}
